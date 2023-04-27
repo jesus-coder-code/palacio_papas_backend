9ebf68ec11c1f59b4110d02e65c027da80caa50a
jesus-coder-code <84207613+jesus-coder-code@users.noreply.github.com>
2023-04-27 03:24:37 -0500

    fix: error de ventas en carro de compras

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -4,41 +4,59 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 const createSale = async (req, res) => {
     try {
-        const {products} = req.body
+        const { products } = req.body
         var total = 0
-        const productSales = products.map(product =>{
-            const {id, quantity} = product
+        const productSales = []
+        for(i of products) {
+            const {id, quantity} = i
+            const productConsult = await prisma.product.findFirst({
+                where:{
+                    id
+                }
+            })
+            //console.log(productConsult)
+            const subtotal = quantity * productConsult.price
+            total += subtotal
+            productSales.push({
+                product: {connect: {id: id}},
+                quantity: quantity,
+                subtotal: subtotal
+            })
+        }
+        /*const productSales = products.map(product => {
+            const { id, quantity } = product
+            console.log(productConsult)
             const subtotal = quantity * product.price
             total += subtotal
             return {
-                product: {connect:{id: id}},
+                product: { connect: { id: id } },
                 quantity: quantity,
                 subtotal: subtotal
             }
-        }) 
+        })*/
 
         const sale = await prisma.sale.create({
-            data:{
+            data: {
                 total: total,
-                products:{
+                products: {
                     create: productSales
                 }
             },
             include: {
                 products: {
-                    include:{
+                    include: {
                         product: true
                     }
                 }
             }
         })
 
-        if(sale){
-            res.json({message:"venta creada"})
-        }else{
-            res.json({message:"no se pudo crear la venta"})
+        if (sale) {
+            res.json({ message: "venta creada" })
+        } else {
+            res.json({ message: "no se pudo crear la venta" })
         }
-        
+
     } catch (error) {
         res.status(500).json({ message: "error interno" })
         console.log(error)
@@ -47,8 +65,12 @@ const createSale = async (req, res) => {
 
 const getSale = async (req, res) => {
     try {
-        const sale = await prisma.sale.findMany()
-        res.status(200).json({data: sale, message: "success"})
+        const sale = await prisma.sale.findMany({
+            include: {
+                products: true
+            }
+        })
+        res.status(200).json({ data: sale, message: "success" })
     } catch (error) {
         res.status(500).json({ message: "error interno" })
         console.log(error)
