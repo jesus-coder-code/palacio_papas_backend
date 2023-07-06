5c69ec9c4cc54dcc6652c65f3fd0909530b1b22e
jesus-coder-code <84207613+jesus-coder-code@users.noreply.github.com>
2023-07-06 18:50:21 -0500

    feat: añadiendo endpoint unico para cajeros en reporte diario

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -6,6 +6,105 @@ const prisma = new PrismaClient()
 
 const dailyReport = async (req, res) => {
     try {
+        const date = req.params.date
+        const courtesies = await prisma.courtesy.findMany({
+            where: {
+                date: new Date(date)
+            }
+        })
+        //res.status(200).send(report)
+
+        const expenses = await prisma.expense.findMany({
+            where: {
+                date: new Date(date)
+            }
+        })
+
+        const payments = await prisma.payment.findMany({
+            where: {
+                date: new Date(date)
+            }
+        })
+
+        const sales = await prisma.sale.findMany({
+            where: {
+                date: new Date(date)
+            },
+            include: {
+                products: {
+                    select: {
+                        product: {
+                            select: {
+                                name: true
+                            }
+                        },
+                        quantity: true
+                    }
+                }
+            }
+        })
+
+        const cashes = await prisma.sale.findMany({
+            where: {
+                date: new Date(date),
+                method: {
+                    equals: 'Cash'
+                },
+            }
+        })
+
+        const transfers = await prisma.sale.findMany({
+            where: {
+                date: new Date(date),
+                method: {
+                    equals: 'Transfer'
+                }
+            }
+        })
+
+        const newdate = new Date(date)
+        const quantityByProduct = await prisma.$queryRaw`SELECT p.name AS productName, SUM(ps.quantity) AS quantitySale FROM Product p INNER JOIN ProductSale ps ON p.id = ps.productId INNER JOIN Sale s ON ps.saleId = s.id WHERE DATE(s.date) = ${newdate} GROUP BY p.name`
+        const dailySale = sales.reduce((total, sale) => total + sale.total, 0)
+        const onCash = cashes.reduce((total, sale) => total + sale.total, 0)
+        const onTransfer = transfers.reduce((total, sale) => total + sale.total, 0)
+        const dailyCourtesy = courtesies.reduce((total, courtesy) => total + courtesy.total, 0)
+        const dailyExpense = expenses.reduce((total, expense) => total + expense.total, 0)
+        const dailyPayment = payments.reduce((total, payment) => total + payment.total, 0)
+
+
+        const discount = {
+            dailyExpense: dailyExpense,
+            dailyPayment: dailyPayment,
+            dailyCourtesy: dailyCourtesy
+        }
+        //const dailySale = onCash + onTransfer
+        let totalDiscount = 0
+        for (let i in discount) {
+            totalDiscount += discount[i]
+        }
+        const dailyReport = [{
+            totalDailySale: dailySale,
+            totalOnCash: onCash,
+            totalOnTransfer: onTransfer,
+            totalDailyExpense: dailyExpense,
+            dailyPayment: dailyPayment,
+            dailyCourtesy: dailyCourtesy,
+            totalDiscount: totalDiscount,
+            quantityByProduct: quantityByProduct
+        }]
+
+        return res.status(200).send([{ message: "success", data: dailyReport, status: "ok" }])
+        //console.log(dailyReport)
+
+    } catch (error) {
+        res.status(500).json({ message: "error interno" })
+        console.log(error)
+
+    }
+}
+
+const reportByCashier = async (req, res) => {
+    try {
         const token = req.headers['verification']
         const date = req.params.date
         const newdate = new Date(date)
@@ -18,176 +117,82 @@ const dailyReport = async (req, res) => {
         req.userRole = auth.role
         const userId = req.userId
         const userRole = req.userRole
-        if (userRole === 'Admin') {
-            const courtesies = await prisma.courtesy.findMany({
-                where: {
-                    date: new Date(date)
-                }
-            })
-            //res.status(200).send(report)
+        const productsByCashier = await prisma.$queryRaw`SELECT u.username AS cashierName, p.name AS productName, SUM(ps.quantity) AS quantitySale FROM Product p INNER JOIN ProductSale ps ON p.id = ps.productId INNER JOIN Sale s ON ps.saleId = s.id INNER JOIN User u ON u.id = s.userId WHERE DATE(s.date) = ${newdate} AND u.id = ${userId} GROUP BY u.id, p.id`
+        const groupResult = {}
 
-            const expenses = await prisma.expense.findMany({
-                where: {
-                    date: new Date(date)
-                }
-            })
-
-            const payments = await prisma.payment.findMany({
-                where: {
-                    date: new Date(date)
-                }
-            })
-
-            const sales = await prisma.sale.findMany({
-                where: {
-                    date: new Date(date)
-                },
-                include: {
-                    products: {
-                        select: {
-                            product: {
-                                select: {
-                                    name: true
-                                }
-                            },
-                            quantity: true
-                        }
-                    }
-                }
-            })
-
-            const cashes = await prisma.sale.findMany({
-                where: {
-                    date: new Date(date),
-                    method: {
-                        equals: 'Cash'
-                    },
-                }
-            })
-
-            const transfers = await prisma.sale.findMany({
-                where: {
-                    date: new Date(date),
-                    method: {
-                        equals: 'Transfer'
-                    }
-                }
-            })
-
-            const newdate = new Date(date)
-            const quantityByProduct = await prisma.$queryRaw`SELECT p.name AS productName, SUM(ps.quantity) AS quantitySale FROM Product p INNER JOIN ProductSale ps ON p.id = ps.productId INNER JOIN Sale s ON ps.saleId = s.id WHERE DATE(s.date) = ${newdate} GROUP BY p.name`
-            const dailySale = sales.reduce((total, sale) => total + sale.total, 0)
-            const onCash = cashes.reduce((total, sale) => total + sale.total, 0)
-            const onTransfer = transfers.reduce((total, sale) => total + sale.total, 0)
-            const dailyCourtesy = courtesies.reduce((total, courtesy) => total + courtesy.total, 0)
-            const dailyExpense = expenses.reduce((total, expense) => total + expense.total, 0)
-            const dailyPayment = payments.reduce((total, payment) => total + payment.total, 0)
-
-
-            const discount = {
-                dailyExpense: dailyExpense,
-                dailyPayment: dailyPayment,
-                dailyCourtesy: dailyCourtesy
+        productsByCashier.forEach((row) => {
+            const { cashierName, productName, quantitySale } = row
+            if (!groupResult[cashierName]) {
+                groupResult[cashierName] = []
             }
-            //const dailySale = onCash + onTransfer
-            let totalDiscount = 0
-            for (let i in discount) {
-                totalDiscount += discount[i]
-            }
-            const dailyReport = [{
-                totalDailySale: dailySale,
-                totalOnCash: onCash,
-                totalOnTransfer: onTransfer,
-                totalDailyExpense: dailyExpense,
-                dailyPayment: dailyPayment,
-                dailyCourtesy: dailyCourtesy,
-                totalDiscount: totalDiscount,
-                quantityByProduct: quantityByProduct
-            }]
+            groupResult[cashierName].push({ product: productName, quantity: quantitySale })
+        })
 
-            return res.status(200).send([{ message: "success", data: dailyReport, status: "ok" }])
-            //console.log(dailyReport)
-        } else if (userRole === 'Cashier') {
-            //reporte diario por cajero
-            const productsByCashier = await prisma.$queryRaw`SELECT u.username AS cashierName, p.name AS productName, SUM(ps.quantity) AS quantitySale FROM Product p INNER JOIN ProductSale ps ON p.id = ps.productId INNER JOIN Sale s ON ps.saleId = s.id INNER JOIN User u ON u.id = s.userId WHERE DATE(s.date) = ${newdate} AND u.id = ${userId} GROUP BY u.id, p.id`
-            const groupResult = {}
-
-            productsByCashier.forEach((row) => {
-                const { cashierName, productName, quantitySale } = row
-                if (!groupResult[cashierName]) {
-                    groupResult[cashierName] = []
-                }
-                groupResult[cashierName].push({ product: productName, quantity: quantitySale })
-            })
-
-            const sales = await prisma.sale.findMany({
-                where: {
-                    userId: userId,
-                    date: newdate
-                },
-                include: {
-                    products: {
-                        select: {
-                            product: {
-                                select: {
-                                    name: true
-                                }
-                            },
-                            quantity: true
-                        }
-                    }
-                }
-            })
-
-            const cashes = await prisma.sale.findMany({
-                where: {
-                    userId: userId,
-                    method: {
-                        equals: 'Cash'
-                    },
-                    date: new Date(date)
-                }
-            })
-            const transfers = await prisma.sale.findMany({
-                where: {
-                    userId: userId,
-                    method: {
-                        equals: 'Transfer'
-                    },
-                    date: new Date(date),
-                }
-            })
-
-            const expenses = await prisma.expense.findMany({
-                where: {
-                    userId: userId,
-                    date: new Date(date)
-                }
-            })
-
-
-            const dailySale = sales.reduce((total, sale) => total + sale.total, 0)
-            const onCash = cashes.reduce((total, sale) => total + sale.total, 0)
-            const onTransfer = transfers.reduce((total, sale) => total + sale.total, 0)
-            const dailyExpense = expenses.reduce((total, expense) => total + expense.total, 0)
-            const dailyReport = [{
-                dailySale: dailySale,
-                onCash: onCash,
-                onTransfer: onTransfer,
-                dailyExpense: dailyExpense
+        const sales = await prisma.sale.findMany({
+            where: {
+                userId: userId,
+                date: newdate
             },
-                groupResult]
-            res.status(200).json({ message: "success", data: dailyReport, status: "ok" })
-        }
+            include: {
+                products: {
+                    select: {
+                        product: {
+                            select: {
+                                name: true
+                            }
+                        },
+                        quantity: true
+                    }
+                }
+            }
+        })
 
+        const cashes = await prisma.sale.findMany({
+            where: {
+                userId: userId,
+                method: {
+                    equals: 'Cash'
+                },
+                date: new Date(date)
+            }
+        })
+        const transfers = await prisma.sale.findMany({
+            where: {
+                userId: userId,
+                method: {
+                    equals: 'Transfer'
+                },
+                date: new Date(date),
+            }
+        })
+
+        const expenses = await prisma.expense.findMany({
+            where: {
+                userId: userId,
+                date: new Date(date)
+            }
+        })
+
+
+        const dailySale = sales.reduce((total, sale) => total + sale.total, 0)
+        const onCash = cashes.reduce((total, sale) => total + sale.total, 0)
+        const onTransfer = transfers.reduce((total, sale) => total + sale.total, 0)
+        const dailyExpense = expenses.reduce((total, expense) => total + expense.total, 0)
+        const dailyReport = [{
+            dailySale: dailySale,
+            onCash: onCash,
+            onTransfer: onTransfer,
+            dailyExpense: dailyExpense
+        },
+            groupResult]
+        res.status(200).json({ message: "success", data: dailyReport, status: "ok" })
     } catch (error) {
-        res.status(500).json({ message: "error interno" })
         console.log(error)
-
+        res.status(500).send({ message: "error interno" })
     }
 }
 
-const reportByCashier = async (req, res) => {
+const productsByCashier = async (req, res) => {
     try {
         const date = req.params.date
         const newdate = new Date(date)
@@ -213,5 +218,6 @@ const reportByCashier = async (req, res) => {
 
 module.exports = {
     dailyReport,
+    productsByCashier,
     reportByCashier
 }
--- routes/report.js
+++ routes/report.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const reports = express.Router()
 const {checkToken, verifyToken, hasRole} = require("../utils/jwt/checkToken")
-const {dailyReport, reportByCashier} = require("../controllers/report.controller")
+const {dailyReport, reportByCashier, productsByCashier} = require("../controllers/report.controller")
+const { check } = require("express-validator")
 
-reports.get("/dailyReport/:date", checkToken, verifyToken, dailyReport)
-reports.get("/reportByCashier/:date", checkToken, verifyToken, hasRole('Admin'), reportByCashier)
-
+reports.get("/dailyReport/:date", checkToken, verifyToken, hasRole('Admin'), dailyReport)
+reports.get("/productsByCashier/:date", checkToken, verifyToken, hasRole('Admin'), productsByCashier)
+reports.get("/reportByCashier/:date", checkToken, verifyToken,hasRole('Cashier'), reportByCashier)
 module.exports = reports
